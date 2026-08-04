5d0e5c2fb90cc09937a1e9a06c97654e993055d0
Kanak Monika Kanphade <kanak.kanphade@gmail.com>
2026-08-04 11:39:31 -0400

    Build basic home page structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://kanakmk.github.io',
   base: '/kanakmk-site',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false,
+  },
 });
